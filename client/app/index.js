c9f69da5bb7d942ea37ac36603d467d9ad452047
Rebecca <rebeccalynn.awesome@gmail.com>
2022-01-20 11:51:50 -0500

    added sql statements and installed fusioncharts

--- client/app/index.js
+++ client/app/index.js
@@ -1,6 +1,22 @@
+
+    // //Including FusionCharts Dependencies
+    // var FusionCharts = require('fusioncharts');
+    // var Charts = require('fusioncharts/fusioncharts.charts');
+    // var FusionTheme = require('fusioncharts/themes/fusioncharts.theme.fusion');
+    // var $ = require('jquery');
+    // var jQueryFusionCharts = require('jquery-fusioncharts');
+    
+    // Charts(FusionCharts); // Resolve Charts as dependency for FusionCharts
+    
+    // FusionTheme(FusionCharts); // Resolve Fusion theme as dependency for FusionCharts
+    
+    // jQueryFusionCharts(FusionCharts); //Resolve jQueryFusionCharts as dependency for FusionCharts    
+
+
 $(document).ready(function(){
 
-
+    // When the value of the dropdown selection changes
+    // Use that value to pass the offset number to the API queryURL
     $("#roster-selection").on("change", function() {
 
         $("#roster-container").html("");
@@ -24,7 +40,7 @@ $(document).ready(function(){
         }
     });
 
-    // Function to GET ten pokemon from API
+    // Function to GET ten pokemon at a time from API
     // Pass offset prop into function for queryURL
     function getPokemon(offset) {
         $("#submit").on("click", function() {
@@ -47,6 +63,7 @@ $(document).ready(function(){
 
                 for (let i = 0; i < results.length; i++) {
                     let studentName = results[i].name;
+                    //let studentType = results[i].type
                     console.log(studentName);
                     renderRoster(studentName);
                 }
@@ -60,9 +77,94 @@ $(document).ready(function(){
     function renderRoster(studentName) {
         const rosterContainer = $("#roster-container");
 
-        let studentNameDiv = $("<span>").html(studentName);
+        let studentNameDiv = $("<p>").html(studentName);
 
         rosterContainer.append(studentNameDiv);
     }
+
+
+
+
+// SAMPLE DATA FOR CHARTING
+
+// Preparing the chart data
+const redRoster = [
+    {
+      label: "Metapod",
+      value: "Bug"
+    },
+    {
+      label: "Butterfree",
+      value: "Bug"
+    },
+    {
+      label: "Weedle",
+      value: "Bug"
+    },
+    {
+      label: "Kakuna",
+      value: "Bug"
+    },
+    {
+      label: "Beedrill",
+      value: "Bug"
+    },
+    {
+      label: "Pidgey",
+      value: "Flying"
+    },
+    {
+      label: "Pidgeotto",
+      value: "Flying"
+    },
+    {
+      label: "Pidgeot",
+      value: "Flying"
+    },
+    {
+      label: "Rattata",
+      value: "Normal"
+    },
+    {
+      label: "Raticate",
+      value: "Normal"
+    }
+  ];
+
+  // Create a JSON object to store the chart configurations
+    const chartConfig = {
+        //Specify the chart type
+        type: "column2d",
+        //Set the container object
+        renderAt: "chart-container",
+        //Specify the width of the chart
+        width: "100%",
+        //Specify the height of the chart
+        height: "400",
+        //Set the type of data
+        dataFormat: "json",
+        dataSource: {
+        chart: {
+            //Set the chart caption
+            caption: "Red's Class Distribution of Pokemon Types",
+            //Set the chart subcaption
+            //subCaption: "",
+            //Set the x-axis name
+            xAxisName: "Country",
+            //Set the y-axis name
+            yAxisName: "Reserves (MMbbl)",
+            numberSuffix: "K",
+            //Set the theme for your chart
+            theme: "fusion"
+        },
+        // Chart Data from Step 2
+        data: redRoster
+        }
+    };
+
+    // FusionCharts.ready(function(){
+    //     var fusioncharts = new FusionCharts(chartConfig);
+    //     fusioncharts.render();
+    // });
 
 });
